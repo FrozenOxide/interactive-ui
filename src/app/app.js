3aa12ab5af9603439a96bb39ea44a9e97744513e
supnoodles <supnoodless@gmail.com>
2022-08-15 01:00:17 +0100

    feat: different views functionality

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,28 @@
 import React, {useState} from 'react';
 import Navbar from './components/navbar.js';
+import { ExperimentsView, CalendarView, MimicView, SettingsView, DesignerView } from './components/views.js'
 
 function App(props) {
   const [currTab, setCurrTab] = useState('experiments');
-
-  const [col, setCol] = useState({
-    col: true,
-  })
+  const currView = 'experiments'
   
   function setParentTab(newTab) {
     setCurrTab(newTab);
   }
   
+  console.log(ExperimentsView)
+  // function ExperimentsView(props) {
+  //   return (<div>EXPERIMENTS</div>);
+  // }
+
   return (
     <div className='content'>
-      <Navbar currTab={currTab} setParentState={setParentTab}/>
+      <Navbar currTab={currTab} setParentState={setParentTab} />
+      {currTab == 'experiments' ? <ExperimentsView/> : null}
+      {currTab == 'calendar' ? <CalendarView /> : null}
+      {currTab == 'mimic' ? <MimicView /> : null}
+      {currTab == 'settings' ? <SettingsView /> : null}
+      {currTab == 'designer' ? <DesignerView/> : null}
     </div>
   );
 }
---
+++ src/app/components/views.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const ExperimentsView = function(props) {
+  return (<div>EXPERIMENTS</div>);
+}
+
+const CalendarView = function(props) {
+  return (<div>CALENDAR</div>);
+}
+
+const MimicView = function(props) {
+  return (<div>MIMIC</div>);
+}
+
+const SettingsView = function(props) {
+  return (<div>SETTINGS</div>);
+}
+
+const DesignerView = function (props) {
+  return (<div>DESIGNER</div>);
+}
+
+export { ExperimentsView, CalendarView, MimicView, SettingsView, DesignerView };
